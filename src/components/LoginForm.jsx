0b32708bcb8e3d84e1d4cf7b9a418abecdbbd939
KongPobW <KongPob.Wisitsak@gmail.com>
2023-11-04 12:26:16 +0700

    draft backend login

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,4 +1,14 @@
+import { useState } from "react";
+
+const handleSubmit = async (username, password) => {
+};
+
 export default function LoginForm() {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
     return (
         <div className="relative flex flex-col items-center justify-center min-h-screen overflow-hidden">
             <div className="w-full p-6 bg-white rounded-md shadow-md lg:max-w-xl">
@@ -13,6 +23,7 @@ export default function LoginForm() {
                         </label>
                         <input
                             type="username"
+                            onChange={e => setUsername(e.target.value)}
                             className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
                         />
                     </div>
@@ -25,11 +36,13 @@ export default function LoginForm() {
                         </label>
                         <input
                             type="password"
+                            onChange={e => setPassword(e.target.value)}
                             className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
                         />
                     </div>
+                    <div className="mt-2 text-rose-500 font-bold">{error}</div>
                     <div className="mt-6">
-                        <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
+                        <button onSubmit={handleSubmit(username, password)} className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
                             Login
                         </button>
                     </div>
---
+++ src/pages/api/login/index.js
@@ -0,0 +1,10 @@
+/* export default async function handler(req, res) {
+    try {
+        await connectMongoDB();
+        const employees = await Employee.find();
+        return res.status(200).json(employees);
+    } catch (error) {
+        console.error("Error fetching employees:", error);
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
+} */
