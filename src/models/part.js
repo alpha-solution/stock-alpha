09b5de7362335cd5b309dcac91ffb7cc79b0e4f3
KongPobW <KongPob.Wisitsak@gmail.com>
2023-11-06 16:06:33 +0700

    may have change

--- src/models/part.js
+++ src/models/part.js
@@ -1,14 +1,14 @@
 import mongoose, { Schema } from "mongoose";
 
 const partSchema = new Schema({
-	part_code: {
-		type: String,
-		required: true
-	},
-	part_name: {
-		type: String,
-		required: true
-	}
+    part_code: {
+        type: String,
+        required: true
+    },
+    part_name: {
+        type: String,
+        required: true
+    }
 });
 
 const Part = mongoose.models.Part || mongoose.model("Part", partSchema);
