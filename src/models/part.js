cc38d53531b58eb43c68d85af566e60a67642b9b
KongPobW <KongPob.Wisitsak@gmail.com>
2023-11-06 16:04:32 +0700

    create part model

---
+++ src/models/part.js
@@ -0,0 +1,16 @@
+import mongoose, { Schema } from "mongoose";
+
+const partSchema = new Schema({
+	part_code: {
+		type: String,
+		required: true
+	},
+	part_name: {
+		type: String,
+		required: true
+	}
+});
+
+const Part = mongoose.models.Part || mongoose.model("Part", partSchema);
+
+export default Part;
