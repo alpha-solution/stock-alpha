3604b4a6c02a026957a2c1624572cf20e0223087
KongPobW <KongPob.Wisitsak@gmail.com>
2023-11-06 22:29:14 +0700

    change something

--- src/utils/connect.js
+++ src/utils/connect.js
@@ -4,7 +4,7 @@ const connectMongoDB = async () => {
 	try {
 		await mongoose.connect(process.env.MONGODB_URI);
 		if (mongoose.connection.readyState) {
-			console.log("Connected to MongoDB.");
+			console.log("Connected to MongoDB");
 		}
 	} catch (error) {
 		console.log(error);
