b7893320342a1ff2f9adb5273e3d5f1627e3462a
KongPobW <KongPob.Wisitsak@gmail.com>
2023-11-10 23:08:04 +0700

    delete draft

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,44 +1,4 @@
 import LoginForm from "@/components/LoginForm";
-// import { useEffect, useState } from "react";
-
-/* const getEmployee = async () => {
-	try {
-		const res = await fetch("http://localhost:3000/api/draft");
-		const data = await res.json();
-		return data;
-	} catch (e) {
-		console.log("Error loading employee: ", e);
-		return null;
-	}
-};
-
-export default function Home() {
-	const [employeeData, setEmployeeData] = useState(null);
-
-	useEffect(() => {
-		const fetchEmployeeData = async () => {
-			const employee = await getEmployee();
-			if (employee) {
-				setEmployeeData(employee);
-			}
-		};
-
-		fetchEmployeeData();
-	}, []);
-
-	return (
-		<div>
-			<div>Hello World</div>
-			{employeeData && (
-				<div>
-					{employeeData.map((em) => (
-						<div key={em.id}>{em.username}</div>
-					))}
-				</div>
-			)}
-		</div>
-	);
-} */
 
 export default function Login() {
 	return <LoginForm />;
