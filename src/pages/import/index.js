0b99b7d06e2d73fb07e86fa657e0c4ea2c05c5f3
KongPobW <KongPob.Wisitsak@gmail.com>
2023-11-11 13:18:40 +0700

    change button text

--- src/pages/import/index.js
+++ src/pages/import/index.js
@@ -1,16 +1,16 @@
 import DataTable from "@/components/DataTable";
 
 export default function Import() {
-    const columnName = ["Import Date", "Part Code", "Part Name", "Amount", "Employee"];
+    const columnTableName = ["Import Date", "Part Code", "Part Name", "Amount", "Employee"];
     return (
         <div className="container mx-auto mt-8 max-w-4xl">
             <div className="flex justify-end">
                 <div>
-                    <button className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">Import</button>
+                    <button className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">+</button>
                     <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 ml-2">Filter</button>
                 </div>
             </div>
-            <DataTable actionButton={false} column={columnName} />
+            <DataTable actionButton={false} columnTable={columnTableName} />
         </div>
     );
 }
