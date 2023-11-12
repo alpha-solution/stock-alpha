46d56d5c27b54691c08e30116e65ca76d8372a57
KongPobW <KongPob.Wisitsak@gmail.com>
2023-11-12 11:22:17 +0700

    make hover button

--- src/components/ActionButton.jsx
+++ src/components/ActionButton.jsx
@@ -1,8 +1,8 @@
 const ActionButton = () => {
     return (
         <td className="py-2" style={{ width: "130px" }}>
-            <button className="mr-2 bg-blue-500 text-white px-2 py-1 rounded">Edit</button>
-            <button className="bg-red-500 text-white px-2 py-1 rounded">Delete</button>
+            <button className="mr-2 bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-600">Edit</button>
+            <button className="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600">Delete</button>
         </td>
     );
 };
