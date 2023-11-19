bb1a2236cccab505bf21f81615b190b978290b95
KongPobW <KongPob.Wisitsak@gmail.com>
2023-11-19 22:00:17 +0700

    remove filter button

--- src/pages/import/index.js
+++ src/pages/import/index.js
@@ -41,7 +41,6 @@ export default function Import() {
             <div className="flex justify-end">
                 <div>
                     <button className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600" onClick={handleAddClicked}>+</button>
-                    <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 ml-2">Filter</button>
                 </div>
             </div>
             <DataTable actionButton={true} columnTable={columnTableName} columnModel={columnModelName} data={imports} />
