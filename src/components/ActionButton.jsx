c1add5605d75201307e47a9cdbb555d2a1565d3f
KongPobW <KongPob.Wisitsak@gmail.com>
2023-11-20 14:51:51 +0700

    edit margin

--- src/components/ActionButton.jsx
+++ src/components/ActionButton.jsx
@@ -2,7 +2,7 @@ const ActionButton = () => {
     return (
         <td className="py-2" style={{ width: "130px" }}>
             <button className="mr-2 bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-600">Edit</button>
-            <button className="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600">Delete</button>
+            <button className="my-1 bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600">Delete</button>
         </td>
     );
 };
